da82f6e98feff08690ad80102d061f9d44bf2d58
André Biseth <andre@biseth.net>
2013-05-30 17:56:14 +0200

    Adding user model

---
+++ Trackifly.Data.Models/User.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Trackifly.Data.Models
+{
+    public class User
+    {
+        public string Id { get; set; }
+        public string Email { get; set; }
+        public DateTime DateCreated { get; set; }
+    }
+}
