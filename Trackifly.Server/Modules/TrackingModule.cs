dca4b47afa6a238d76d5d1c7a0353cdefe6936e6
André Biseth <andre@biseth.net>
2013-06-04 19:31:33 +0200

    Adding http methods

--- Trackifly.Server/Modules/TrackingModule.cs
+++ Trackifly.Server/Modules/TrackingModule.cs
@@ -7,32 +7,31 @@ using Trackifly.Data.Models;
 using Trackifly.Data.Storage;
 using Trackifly.Server.Configuration;
 using Trackifly.Server.Helpers;
-using Trackifly.Server.Models;
 
 namespace Trackifly.Server.Modules
 {
     public class TrackingModule : BaseModule
     {
-        private static readonly Dictionary<string,DateTime> SessionCache = new Dictionary<string, DateTime>();
+        private static readonly Dictionary<string, DateTime> SessionCache = new Dictionary<string, DateTime>();
         private readonly TrackingSessions _trackingSessions;
 
-        public TrackingModule(IDataStore dataStore, TrackingSessions trackingSessions, ErrorCodes errorCodes) : base(dataStore, errorCodes)
+        public TrackingModule(IDataStore dataStore, TrackingSessions trackingSessions, ErrorCodes errorCodes)
+            : base(dataStore, errorCodes)
         {
             _trackingSessions = trackingSessions;
             Get["/tracking/{sessionid}"] = parameters =>
                 {
                     string sessionId = parameters.sessionId;
-                    if(sessionId != null)
-                    {
-                        var trackingSession = _trackingSessions.Get(sessionId);
-                        if(trackingSession == null)
-                            return ErrorResponse(HttpStatusCode.NotFound);
-                        if (trackingSession.Expires >= DateTime.Now)
-                            return ErrorResponse(HttpStatusCode.Forbidden, "Session has expired!");
+                    if (sessionId == null)
+                        return ErrorResponse(HttpStatusCode.NotFound);
+                    
+                    var trackingSession = _trackingSessions.Get(sessionId);
+                    if (trackingSession == null)
+                        return ErrorResponse(HttpStatusCode.NotFound);
+                    if (trackingSession.Expires >= DateTime.Now)
+                        return ErrorResponse(HttpStatusCode.Forbidden, "Session has expired!");
 
-                        return Response.AsJson(trackingSession);
-                    }
-                    return ErrorResponse(HttpStatusCode.NotFound);
+                    return Response.AsJson(trackingSession);
                 };
             Post["/tracking/"] = parameters =>
                 {
@@ -41,15 +40,49 @@ namespace Trackifly.Server.Modules
                     if (SessionCache.TryGetValue(ip, out createdDate))
                     {
                         if (createdDate.AddSeconds(AppSettings.GlobalSaveRetention) > DateTime.Now)
-                            return ErrorResponse(HttpStatusCode.TooManyRequests, string.Format("Please wait for at least {0} seconds before you create a new session.", AppSettings.GlobalSaveRetention));
+                            return ErrorResponse(HttpStatusCode.TooManyRequests,
+                                                 string.Format(
+                                                     "Please wait for at least {0} seconds before you create a new session.",
+                                                     AppSettings.GlobalSaveRetention));
                     }
                     SessionCache[ip] = DateTime.Now;
 
                     var trackingSession = this.Bind<TrackingSession>();
-                    
+
                     _trackingSessions.Add(trackingSession);
 
                     return Response.AsJson(trackingSession);
+                };
+            Put["/tracking/{sessionid}"] = parameters =>
+                {
+                    var ip = Request.UserHostAddress;
+                    DateTime createdDate;
+                    if (SessionCache.TryGetValue(ip, out createdDate))
+                    {
+                        if (createdDate.AddSeconds(AppSettings.GlobalSaveRetention) > DateTime.Now)
+                            return ErrorResponse(HttpStatusCode.TooManyRequests,
+                                                 string.Format(
+                                                     "Please wait for at least {0} seconds before you create a new session.",
+                                                     AppSettings.GlobalSaveRetention));
+                    }
+                    SessionCache[ip] = DateTime.Now;
+
+                    var trackingSession = this.Bind<TrackingSession>();
+
+                    _trackingSessions.Add(trackingSession);
+
+                    return Response.AsJson(trackingSession);
+                };
+            Delete["/tracking/{sessionid}"] = parameters =>
+                {
+                    string sessionId = parameters.sessionId;
+                    if (sessionId != null)
+                    {
+                        _trackingSessions.Delete(sessionId);
+                        return ErrorResponse(HttpStatusCode.OK,
+                                             string.Format("Tracking session {0} is removed.", sessionId));
+                    }
+                    return ErrorResponse(HttpStatusCode.BadRequest);
                 };
         }
     }
